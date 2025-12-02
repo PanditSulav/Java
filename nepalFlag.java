600121ebcaba38d53c4b399d0124dad559bcb57c
SULABH PANDIT <144801167+PanditSulav@users.noreply.github.com>
2025-12-02 11:24:48 +0545

    Add files via upload

---
+++ nepalFlag.java
@@ -0,0 +1,510 @@
+import java.util.Scanner;
+
+public class nepalFlag {
+    public static void main(String[] args) {
+      
+        Scanner scanner = new Scanner(System.in);
+        char choice;
+        String operator;
+        int i,j;
+        do {
+          System.out.println("Enter any key for make Nepal flag The key must be with in *,+,-,/,0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F");
+          operator = scanner.next();
+            switch (operator) {
+case "*":
+                    for(i=1;i<=5;i++)
+                    {
+                        for(j=1;j<=i;j++)
+                        {
+                        
+                        System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+
+  
+                    for(i=1;i<=5;i++)
+                    {
+                        for(j=1;j<=i;j++)
+                        {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    for(i=6;i<=13;i++)
+                    {
+                        System.out.println("*");
+                }
+                    break;
+case "+":
+                    for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("+");
+                    }
+                    System.out.println();
+                }
+
+            
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("+");
+                    }
+                    System.out.println();
+                }
+                for(i=6;i<=13;i++)
+                {
+                    System.out.println("+");
+                }
+                    break;
+
+case "-":
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("-");
+                    }
+                    System.out.println();
+                }
+
+            
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("-");
+                    }
+                    System.out.println();
+                }
+                for(i=6;i<=13;i++)
+                {
+                    System.out.println("-");
+            }
+            break;
+
+case "/":
+                for(i=1;i<=5;i++)
+            {
+                for(j=1;j<=i;j++)
+                {
+                    System.out.print("/");
+                }
+                System.out.println();
+            }
+
+        
+            for(i=1;i<=5;i++)
+            {
+                for(j=1;j<=i;j++)
+                {
+                    System.out.print("/");
+                }
+                System.out.println();
+            }
+            for(i=6;i<=13;i++)
+            {
+                System.out.println("/");
+            }
+                    break;
+case "0":
+                for(i=1;i<=5;i++)
+                {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("0");
+            }
+            System.out.println();
+            }
+
+        
+            for(i=1;i<=5;i++)
+            {
+                for(j=1;j<=i;j++)
+                {
+                    System.out.print("0");
+                }
+                System.out.println();
+            }
+            for(i=6;i<=13;i++)
+            {
+                System.out.println("0");
+            }
+                            break;
+case "1":
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("1");
+                    }
+                    System.out.println();
+                }
+
+            
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("1");
+                    }
+                    System.out.println();
+                }
+                for(i=6;i<=13;i++)
+                {
+                    System.out.println("1");
+            }
+                                break;
+case "2":
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("2");
+                    }
+                    System.out.println();
+                }
+
+            
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("2");
+                    }
+                    System.out.println();
+                }
+                for(i=6;i<=13;i++)
+                {
+                    System.out.println("2");
+            }
+                        break;
+case "3":
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("3");
+                    }
+                    System.out.println();
+                }
+
+            
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("3");
+                    }
+                    System.out.println();
+                }
+                for(i=6;i<=13;i++)
+                {
+                    System.out.println("3");
+            }
+                        break;
+
+case "4":
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("4");
+                    }
+                    System.out.println();
+                }
+
+            
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("4");
+                    }
+                    System.out.println();
+                }
+                for(i=6;i<=13;i++)
+                {
+                    System.out.println("4");
+            }
+                break;
+case "5":
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("5");
+                    }
+                    System.out.println();
+                }
+
+            
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("5");
+                    }
+                    System.out.println();
+                }
+                for(i=6;i<=13;i++)
+                {
+                    System.out.println("5");
+            }
+                break;
+case "6":
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("6");
+                    }
+                    System.out.println();
+                }
+
+            
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("6");
+                    }
+                    System.out.println();
+                }
+                for(i=6;i<=13;i++)
+                {
+                    System.out.println("6");
+            }
+                break;
+                
+case "7":
+            for(i=1;i<=5;i++)
+            {
+                for(j=1;j<=i;j++)
+                {
+                    System.out.print("7");
+                }
+                System.out.println();
+            }
+
+        
+            for(i=1;i<=5;i++)
+            {
+                for(j=1;j<=i;j++)
+                {
+                    System.out.print("7");
+                }
+                System.out.println();
+            }
+            for(i=6;i<=13;i++)
+            {
+                System.out.println("7");
+        }
+            break;                            
+                
+case "8":
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("8");
+                    }
+                    System.out.println();
+                }
+
+            
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("8");
+                    }
+                    System.out.println();
+                }
+                for(i=6;i<=13;i++)
+                {
+                    System.out.println("8");
+            }
+            break;
+case "9":
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("9");
+                    }
+                    System.out.println();
+                }
+
+            
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("9");
+                    }
+                    System.out.println();
+                }
+                for(i=6;i<=13;i++)
+                {
+                    System.out.println("9");
+            }
+                break;
+case "A":
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("A");
+                    }
+                    System.out.println();
+                }
+
+            
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("A");
+                    }
+                    System.out.println();
+                }
+                for(i=6;i<=13;i++)
+                {
+                    System.out.println("A");
+            }
+            break;
+case "B":
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("B");
+                    }
+                    System.out.println();
+                }
+
+            
+                for(i=1;i<=5;i++)                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("B");
+                    }
+                    System.out.println();
+                }
+                for(i=6;i<=13;i++)
+                {
+                    System.out.println ("B");
+            }
+                break;
+
+case "C":
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("C");
+                    }
+                    System.out.println();
+                }
+
+            
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("C");
+                    }
+                    System.out.println();
+                }
+                for(i=6;i<=13;i++)
+                {
+                    System.out.println("C");
+            }
+            break;
+case "D":
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("D");
+                    }
+                    System.out.println();
+                }
+
+            
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("D");
+                    }
+                    System.out.println();
+                }
+                for(i=6;i<=13;i++)
+                {
+                    System.out.println("D");
+            }
+                        break;
+case "E":
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("E");
+                    }
+                    System.out.println();
+                }
+
+            
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("E");
+                    }
+                    System.out.println();
+                }
+                for(i=6;i<=13;i++)
+                {
+                    System.out.println("E");
+            }
+            break;
+case "F":
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("F");
+                    }
+                    System.out.println();
+                }
+
+            
+                for(i=1;i<=5;i++)
+                {
+                    for(j=1;j<=i;j++)
+                    {
+                        System.out.print("F");
+                    }
+                    System.out.println();
+                }
+                for(i=6;i<=13;i++)
+                {
+                    System.out.println("F");
+            }
+             break;
+
+
+                default: System.out.println("Nothing");
+                    break;
+                }
+
+             System.out.print("Do another calculation? (y/n): ");
+            choice = scanner.next().charAt(0);
+    }while (choice == 'y' || choice == 'Y');
+}
+}
